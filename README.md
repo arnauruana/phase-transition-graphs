1686419eeca70defc85206eec5733da2f63b7d3e
Arnau Ruana <arnauruana@outlook.com>
2019-09-23 12:26:44 +0200

    BinomialRandomGraph acabat + utilitats varies al makefile i README

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Transició de fase i components connexes en grafs aleatoris.
 * Tutorial doctest: https://accu.org/index.php/journals/2343
 * Binamial Random Graphs: https://en.wikipedia.org/wiki/Erd%C5%91s%E2%80%93R%C3%A9nyi_model
 * Random Geometric Graphs: https://en.wikipedia.org/wiki/Random_geometric_graph
+* [GraphViz Online Generator:](http://webgraphviz.com/)
 
 ## Compilar i executar
 
 El projecte es pot compilar amb G++ i Make i s'executa normalment.
-
--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,71 @@
+#include <cstdlib>
 #include <string>
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#include <set>
+using std::set;
+
+#include <utility>
+using std::pair;
+using std::make_pair;
 
 #define DOCTEST_CONFIG_IMPLEMENT
 #define DEBUG 0
 #include "doctest.h"
 #include "graph.h"
+
+
+set<size_t> verts(const size_t n) {
+  set<size_t> verts;
+  for (size_t i = 0; i < n; ++i) {
+    verts.insert(i);
+  }
+  return verts;
+}
+
+Graph BinomialRandomGraph(const size_t n, double p) {
+  if (p < 0) p = 0;
+  else if (p > 1) p = 1;
+  p *= 100;
+  double countIF = 0; // DEBUG
+  double countEL = 0; // DEBUG
+  set<pair<size_t, size_t>> edges;
+  for (size_t i = 0; i < n; ++i) {
+    for (size_t j = i + 1; j < n; ++j) {
+      double x = rand() % 100;
+      if (x < p) {
+        edges.insert(make_pair(i,j));
+        ++countIF; // DEBUG
+        cout << "IF \t" << x << "\t" << p << endl; // DEBUG
+      }
+      else { // DEBUG
+        ++countEL; // DEBUG
+        cout << "ELSE \t" << x << "\t" << p << endl; // DEBUG
+      }
+    }
+  }
+  cout << endl; // DEBUG
+  cout << countIF << '\t' << (countIF/(countIF+countEL))*100. << endl; // DEBUG
+  cout << countEL << '\t' << (countEL/(countIF+countEL))*100. << endl << endl; // DEBUG
+  return Graph(verts(n), edges);
+}
+
+// DEBUG {
+  inline void debugBRG() {
+    cout << endl << "=============================" << endl << endl;
+    int n;
+    double p;
+    while (cin >> n >> p) {
+      Graph BRG = BinomialRandomGraph(n, p);
+      BRG.print();
+      cout << endl;
+    }
+  }
+// } DEBUG
 
 int main(int argc, char** argv) {
     doctest::Context ctx;
@@ -12,6 +74,7 @@ int main(int argc, char** argv) {
     int res = 0;
     if (DEBUG || (argc > 1 && std::string(argv[1]) == "debug")) {
         res = ctx.run();
+        debugBRG(); // tests for BinomialRandomGraph function
         return res;
     }
 
